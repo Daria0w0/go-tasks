6c6c16c9433b54891139bb5bfd2261736647c563
Daria0w0 <niko.roo77@gmail.com>
2025-06-29 01:28:41 +0300

    bug fix

--- hello/hello.go
+++ hello/hello.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	name := "Daria"
 	fmt.Println("Hello,", name)
-	fmt.Println("Today is", time.Now().Format("19.06.2025"))
+	fmt.Println("Today is", time.Now().Format("02.01.2006"))
 }
